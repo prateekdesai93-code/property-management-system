15d6436d1c6f36a9993a54d6f2cac244c5e0d74a
Pratik Desai <pratikdesai@Pratiks-MacBook-Pro.local>
2026-08-17 08:08:12 +0200

    Add static export for GitHub Pages

---
+++ docs/dev/server/server-reference-manifest.js
@@ -0,0 +1,1 @@
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"1Ie04mt4Ot7QlwD6DNZFMjGYFZal24wh9yR+qrCPPog=\"\n}"
---
+++ next.config.ts
@@ -0,0 +1,9 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  output: "export",
+  basePath: "/property-management-system",
+  distDir: "docs",
+};
+
+export default nextConfig;
